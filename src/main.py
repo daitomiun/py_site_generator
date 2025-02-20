854a5b8a6ad4a046f64c91a94dc2d1341cb573ec
daitomiun <mateo.rodriguez.fierro@gmail.com>
2025-02-19 21:22:39 -0500

    Split images and links added

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from leafnode import LeafNode
-from md_text_to_text import extract_markdown_images, extract_markdown_links, split_nodes_delimiter
+from md_text_to_text import extract_markdown_images, extract_markdown_links, split_nodes_delimiter, split_nodes_image, split_nodes_link
 from parentnode import ParentNode
 from text_to_html import text_node_to_html_node
 from textnode import TextNode, TextType
@@ -76,7 +76,6 @@ def main():
     # - TextNode("", TextType.BOLD)      # Empty after bold content
     # - TextNode(" text", TextType.TEXT)
     print(result)
-    '''
     text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
     print(extract_markdown_images(text))
     # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
@@ -89,5 +88,31 @@ def main():
 
     text = "This is text with a ![rick [] roll](https://i.imgur.com/aKaOqIh.gif []) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
     print(extract_markdown_images(text))
- 
+    '''
+    node = TextNode(
+        "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev) and",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_link([node])
+    print(new_nodes)
+    # [
+    #     TextNode("This is text with a link ", TextType.TEXT),
+    #     TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+    #     TextNode(" and ", TextType.TEXT),
+    #     TextNode(
+    #         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+    #     ),
+    # ]
+    node = TextNode(
+        "This is text with an image ![cute bear](https://example.com/bear.jpg) and some text after it",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_image([node])
+    print(new_nodes)
+    # Expected result would be something like:
+    # [
+    #     TextNode("This is text with an image ", TextType.TEXT),
+    #     TextNode("cute bear", TextType.IMAGE, "https://example.com/bear.jpg"),
+    #     TextNode(" and some text after it", TextType.TEXT),
+    # ]
 main()
--- src/md_text_to_text.py
+++ src/md_text_to_text.py
@@ -32,3 +32,57 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text) 
 
+def split_nodes_image(old_nodes):
+    return get_split_nodes(
+        old_nodes,
+        TextType.IMAGE,
+        extract_markdown_images,
+        "![", "]", "(", ")"
+    )
+def split_nodes_link(old_nodes):
+    return get_split_nodes(
+        old_nodes,
+        TextType.LINK,
+        extract_markdown_links,
+        "[", "]", "(", ")"
+    )
+
+def get_split_nodes(old_nodes, text_type, extract, left_delimiter, right_delimiter, url_left, url_right):
+    """Split text nodes based on markdown patterns for links or images.
+    
+    Args:
+        old_nodes (list): List of TextNode objects to process
+        text_type (TextType): Type of node to create (LINK or IMAGE)
+        extract (function): Function to extract markdown content (links or images)
+        left_delimiter (str): Opening delimiter (e.g. "[" or "![")
+        right_delimiter (str): Closing delimiter for alt text ("]")
+        url_left (str): Opening delimiter for URL ("(")
+        url_right (str): Closing delimiter for URL (")")
+    
+    Returns:
+        list: New list of TextNode objects with markdown content split into separate nodes
+    
+    Example:
+        >>> nodes = [TextNode("Hello [world](url)", TextType.TEXT)]
+        >>> get_split_nodes(nodes, TextType.LINK, extract_markdown_links, "[", "]", "(", ")")
+        [TextNode("Hello ", TextType.TEXT), TextNode("world", TextType.LINK, "url")]
+    """
+    new_nodes = []
+    for node in old_nodes:
+        current_text = node.text
+        while current_text != "":
+            links = extract(current_text) # <-- method for the extraction type
+            if len(links) == 0:
+                new_nodes.append(TextNode(current_text, TextType.TEXT))
+                break
+
+            alt, url = links[0]
+            sections = current_text.split(f"{left_delimiter}{alt}{right_delimiter}{url_left}{url}{url_right}", 1)
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(alt, text_type, url))
+            current_text = sections[1]
+
+    return new_nodes
+
